4e8bb4397f3e40b94d6049a13654ed98392c98a2
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-01-17 13:06:20 +0500

    push

--- app/Http/Controllers/API/v1/AgoraController.php
+++ app/Http/Controllers/API/v1/AgoraController.php
@@ -4,25 +4,23 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Willywes\AgoraSDK\RtcTokenBuilder;
 
 class AgoraController extends Controller
 {
-    public function rtc_token(Request $request)
-    {
-        $appID = '03db78a190bc4764b5a4ea2f0c119294';
-        $appCertificate = '7bb4f4c5eb1246cdb0557858fa751500';
-
+    public static function rtc_token(Request $request){
+    
+        $appID = "03db78a190bc4764b5a4ea2f0c119294";
+        $appCertificate = "7bb4f4c5eb1246cdb0557858fa751500";
         $channelName = $request->channelName;
-        $user = $request->uid;
+        $uid = $request->uid;
+        $uidStr = ($request->uid) . '';
         $role = RtcTokenBuilder::RoleAttendee;
-        $expireTimeInSeconds = 36000;
-        $currentTimestamp = now()->getTimestamp();
+        $expireTimeInSeconds = 3600;
+        $currentTimestamp = (new \DateTime("now", new \DateTimeZone('UTC')))->getTimestamp();
         $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
-
-        $key = RtcTokenBuilder::buildKey($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
-        $rtcToken = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
-
-        // Return or use $rtcToken as needed
-        return response()->json(['rtcToken' => $rtcToken]);
+    
+        return RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $uid, $role, $privilegeExpiredTs);
+    
     }
 }
