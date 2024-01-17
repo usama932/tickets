bd638cdbe839d9ead57222d90b679cba8687f04b
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-01-17 11:50:30 +0500

    agora

---
+++ app/Http/Controllers/API/v1/AgoraController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AgoraController extends Controller
+{
+    public function rtc_token(Request $request){
+        $appID = '03db78a190bc4764b5a4ea2f0c119294';
+        $appCertificate = '7bb4f4c5eb1246cdb0557858fa751500';
+
+        $channelName = $request->channelName;
+        $user = Auth::user()->name;
+        $role = RtcTokenBuilder::RoleAttendee;
+        $expireTimeInSeconds = 36000;
+        $currentTimestamp = now()->getTimestamp();
+        $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
+
+        $rtcToken = RtcTokenBuilder::buildTokenWithUserAccount($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
+    }
+}
