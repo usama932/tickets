c2bbe329d542b5e11d045a192ef726e69a131164
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-01-17 12:00:00 +0500

    Update AgoraController.php

--- app/Http/Controllers/API/v1/AgoraController.php
+++ app/Http/Controllers/API/v1/AgoraController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers\API\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Agora\Token\RtcTokenBuilder;
 
 class AgoraController extends Controller
 {
-    public function rtc_token(Request $request){
+    public function rtc_token(Request $request)
+    {
         $appID = '03db78a190bc4764b5a4ea2f0c119294';
         $appCertificate = '7bb4f4c5eb1246cdb0557858fa751500';
 
@@ -18,6 +20,10 @@ class AgoraController extends Controller
         $currentTimestamp = now()->getTimestamp();
         $privilegeExpiredTs = $currentTimestamp + $expireTimeInSeconds;
 
-        $rtcToken = RtcTokenBuilder::buildTokenWithUserAccount($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
+        $key = RtcTokenBuilder::buildKey($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
+        $rtcToken = RtcTokenBuilder::buildTokenWithUid($appID, $appCertificate, $channelName, $user, $role, $privilegeExpiredTs);
+
+        // Return or use $rtcToken as needed
+        return response()->json(['rtcToken' => $rtcToken]);
     }
 }
