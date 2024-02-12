f4f13ca6d40424cdc90081740413a199cd3e4cc6
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-02-12 22:33:53 +0500

    Update DocumentsController.php

--- app/Http/Controllers/API/v1/DocumentsController.php
+++ app/Http/Controllers/API/v1/DocumentsController.php
@@ -277,6 +277,7 @@ class DocumentsController extends Controller
 
     }
     public function cpvRequirement(Request $request){
+       
         if($request->cpv_id != 0){
             $cpvs = CpvRequirement::where('id',$request->cpv_id)->update([
                 'driver_fatigue' => $request->driver_fatigue,
@@ -291,6 +292,7 @@ class DocumentsController extends Controller
                 'driver_id' => $request->driver_id,
             ]);
         }else{
+            
             $cpvs = CpvRequirement::create([
                 'driver_fatigue' => $request->driver_fatigue,
                 'drug_alcohol' => $request->drug_alcohol,
@@ -303,11 +305,13 @@ class DocumentsController extends Controller
                 'reporting_hazards' => $request->reporting_hazards,
                 'driver_id' => $request->driver_id,
             ]);
+           
         }
         $response['success'] = 'success';
         $response['error'] = null;
         $response['message'] = 'successfully';
         $response['cpvs'] = $cpvs;
+        return response()->json($response);
     }
 
 
