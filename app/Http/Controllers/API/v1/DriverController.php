83df3a7234fd81faac4eb5b9912eec31bc74f1f2
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2024-03-17 11:33:11 +0500

    Update DriverController.php

--- app/Http/Controllers/API/v1/DriverController.php
+++ app/Http/Controllers/API/v1/DriverController.php
@@ -34,16 +34,14 @@ class DriverController extends Controller
 
     public function getData(Request $request)
     {
-
         $lat1 = $request->get('lat1');
         $lng1 = $request->get('lng1');
         $lat2 = $request->get('lat2');
         $lng2 = $request->get('lng2');
+    
         $sql = DB::table('tj_type_vehicule')
             ->crossJoin('tj_vehicule')
             ->crossJoin('delivery_charges')
-            // ->leftJoin('brands', 'brands.id', '=', 'tj_vehicule.brand')
-            
             ->crossJoin('tj_conducteur')
             ->leftJoin('cpv_requirement', 'cpv_requirement.driver_id', '=', 'tj_conducteur.id')
             ->leftJoin('remaining_tokens', 'remaining_tokens.user_id', '=', 'tj_conducteur.id')
@@ -55,80 +53,78 @@ class DriverController extends Controller
             ->where('tj_vehicule.id_type_vehicule', '=', DB::raw('tj_type_vehicule.id'))
             ->where('delivery_charges.id_vehicle_type', '=', DB::raw('tj_vehicule.id_type_vehicule'))
             ->where('tj_vehicule.id_conducteur', '=', DB::raw('tj_conducteur.id'))
-            ->where('tj_vehicule.statut', '=', 'yes')->where('tj_conducteur.statut', '=', 'yes')
-            ->where('tj_conducteur.is_verified', '=', '1')->where('tj_conducteur.online', '!=', 'no')
+            ->where('tj_vehicule.statut', '=', 'yes')
+            ->where('tj_conducteur.statut', '=', 'yes')
+            ->where('tj_conducteur.is_verified', '=', '1')
+            ->where('tj_conducteur.online', '!=', 'no')
             ->where('tj_type_vehicule.status', '=', 'yes')
-            ->where('tj_conducteur.latitude', '!=', '')->where('tj_conducteur.longitude', '!=', '')
+            ->where('tj_conducteur.latitude', '!=', '')
+            ->where('tj_conducteur.longitude', '!=', '')
             ->where('cpv_requirement.updated_at', '>', Carbon::now()->subMonths(6))
             ->where('remaining_tokens.tokens', '>', 0)
-            //->groupBy('tj_conducteur.id')
             ->get();
-
-//		}
-        //dd($sql);
+    
         $allDistance = array();
+    
         if ($sql->count() > 0) {
             $output = array();
             foreach ($sql as $row) {
-
                 $id_conducteur = $row->id;
-                if ($row->latitude != '' && $row->longitude != '')
+                if ($row->latitude != '' && $row->longitude != '') {
                     $row->distance = DriverController::distance($row->latitude, $row->longitude, $lat1, $lng1);
+                }
                 $row->destinationDistance = DriverController::distance($lat1, $lng1, $lat2, $lng2);
                 $row->unit = 'KM';
                 $allDistance[] = $row->distance;
-                $sql_nb_avis = DB::table('tj_note')->select(DB::raw("COUNT(id) as nb_avis"), DB::raw("SUM(niveau) as somme"))->where('id_conducteur', '=', DB::raw($row->idVehicule))->get();
-                $row->price = round($row->destinationDistance * floatval($row->prix), 0);
-                if (!empty($sql_nb_avis)) {
-
-                    foreach ($sql_nb_avis as $row_nb_avis) {
-
-                        $somme = $row_nb_avis->somme;
-                        $nb_avis = $row_nb_avis->nb_avis;
-
-                        if ($nb_avis != 0) {
-                            $moyenne = $somme / $nb_avis;
-                        } else {
-                            $moyenne = 0;
+                
+                if($row->distance <= 5){
+                    $sql_nb_avis = DB::table('tj_note')->select(DB::raw("COUNT(id) as nb_avis"), DB::raw("SUM(niveau) as somme"))->where('id_conducteur', '=', DB::raw($row->idVehicule))->get();
+    
+                    $row->price = round($row->destinationDistance * floatval($row->prix), 0);
+        
+                    if (!empty($sql_nb_avis)) {
+                        foreach ($sql_nb_avis as $row_nb_avis) {
+                            $somme = $row_nb_avis->somme;
+                            $nb_avis = $row_nb_avis->nb_avis;
+                            if ($nb_avis != 0) {
+                                $moyenne = $somme / $nb_avis;
+                            } else {
+                                $moyenne = 0;
+                            }
                         }
-                    }
-                } else {
-                    $somme = 0;
-                    $nb_avis = 0;
-                    $moyenne = 0;
-                }
-
-                $row->moyenne = $moyenne;
-
-                $sql_total = DB::table('tj_requete')->select(DB::raw("COUNT(id) as total_completed_ride"))->where('id_conducteur', '=', DB::raw($id_conducteur))->where('statut', '=', 'completed')->get();
-
-                foreach ($sql_total as $row_total) {
-                    $row->total_completed_ride = $row_total->total_completed_ride;
-                }
-
-                if ($row->photo != '') {
-
-                    if ($row->photo) {
-                        $image_user = asset('assets/images/driver') . '/' . $row->photo;
                     } else {
-                        $image_user = asset('assets/images/placeholder_image.jpg');
-
+                        $somme = 0;
+                        $nb_avis = 0;
+                        $moyenne = 0;
                     }
-                    $row->photo = $image_user;
+                    $row->moyenne = $moyenne;
+                    $sql_total = DB::table('tj_requete')->select(DB::raw("COUNT(id) as total_completed_ride"))->where('id_conducteur', '=', DB::raw($id_conducteur))->where('statut', '=', 'completed')->get();
+        
+                    foreach ($sql_total as $row_total) {
+                        $row->total_completed_ride = $row_total->total_completed_ride;
+                    }
+                    if ($row->photo != '') {
+                        if ($row->photo) {
+                            $image_user = asset('assets/images/driver') . '/' . $row->photo;
+                        } else {
+                            $image_user = asset('assets/images/placeholder_image.jpg');
+                        }
+                        $row->photo = $image_user;
+                    }
+                    if ($row->distance <= 20) {
+                        $output[] = $row;
+                    }
                 }
-                if ($row->distance <= 20) {
-                    $output[] = $row;
-                }
-
             }
-
+    
             function cmp($a, $b)
             {
-                if ($a->distance == $b->distance)
+                if ($a->distance == $b->distance) {
                     return 0;
+                }
                 return ($a->distance < $b->distance) ? -1 : 1;
             }
-
+    
             if (!is_null($output)) {
                 usort($output, 'App\Http\Controllers\API\v1\cmp');
             }
@@ -138,7 +134,7 @@ class DriverController extends Controller
                 $response['message'] = 'Successfully fetched data';
                 $response['data'] = $output;
                 return response()->json($response);
-            }else {
+            } else {
                 $response['success'] = 'Success';
                 $response['error'] = null;
                 $response['message'] = 'No driver found';
@@ -152,22 +148,19 @@ class DriverController extends Controller
             $response['data'] = null;
             return response()->json($response, 422);
         }
-
     }
-
-    // public static function cmp($a,$b){
+    
+    public static function distance($lat1, $lng1, $lat2, $lng2)
+    {
+        $rad = M_PI / 180;
+        return acos(sin($lat2 * $rad) * sin($lat1 * $rad) + cos($lat2 * $rad) * cos($lat1 * $rad) * cos($lng2 * $rad - $lng1 * $rad)) * 6371; // Kilometers
+    }
+    
+   // public static function cmp($a,$b){
     //   if ($a["distance"]==$b["distance"])
     //       return 0;
     //       return ($a["distance"] < $b["distance"])?-1:1;
     // }
-
-    public static function distance($lat1, $lng1, $lat2, $lng2)
-    {
-        $rad = M_PI / 180;
-        return acos(sin($lat2 * $rad) * sin($lat1 * $rad) + cos($lat2 * $rad) * cos($lat1 * $rad)
-                * cos($lng2 * $rad - $lng1 * $rad)) * 6371;// Kilometers
-    }
-
     public function changeStatus(Request $request)
     {
         $table = DB::table('tj_conducteur')->where('id', '=', $request->get('tj_conducteur'))->first();
